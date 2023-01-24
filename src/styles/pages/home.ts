e25ac31b3aafc870638fa451942b49f98802bafb
Gabriel Cesarino <61214104+gabrielCesarino@users.noreply.github.com>
2023-01-24 12:58:23 -0300

    style: carroussel functionality with keen-slider

--- src/styles/pages/home.ts
+++ src/styles/pages/home.ts
@@ -3,8 +3,8 @@ import { styled } from "..";
 
 export const HomeContainer = styled('main', {
     display: 'flex',
-    gap: '3rem',
-    maxWidth: 'calc(100vw - ((100vw - 1180) / 2))',
+    width: '100%',
+    maxWidth: 'calc(100vw - ((100vw - 1180px) / 2))',
     marginLeft: "auto",
     minHeight: 656,
 })
@@ -12,11 +12,9 @@ export const HomeContainer = styled('main', {
 export const Product = styled('a', {
     background: 'linear-gradient(100deg, #1ea484 0%, #7465d4 100%)',
     borderRadius: 8,
-    padding: '0.25rem',
     cursor: 'pointer',
     position: 'relative',
     overflow: 'hidden',
-    
     display: 'flex',
     justifyContent: "center",
     alignItems: "center",
