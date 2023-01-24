2e8650192a68d99c68683177cbabbaa0f5b015b6
Gabriel Cesarino <61214104+gabrielCesarino@users.noreply.github.com>
2023-01-23 21:05:21 -0300

    styles: globalStyle config

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,28 @@
+import { styled } from "../styles"
+
+const Button = styled('button', {
+  backgroundColor: '$green300',
+  borderRadius: 8,
+  outline: 0,
+  border: 0,
+  padding: '4px 8px',
+
+  span: {
+    fontWeight: 'bold',
+  },
+
+  '&:hover': {
+    cursor: 'pointer',
+  }
+})
+
 export default function Home() {
   return (
     <>
-      <h1>Hello world</h1>
+      <Button>
+        <span>Teste</span>
+        Hello world
+      </Button>
     </>
   )
 }
