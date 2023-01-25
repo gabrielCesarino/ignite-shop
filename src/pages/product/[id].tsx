d3e048900616fe5a3d036954aa3fdfd4e7f4b31b
Gabriel Cesarino <61214104+gabrielCesarino@users.noreply.github.com>
2023-01-25 18:16:24 -0300

    style: product page style structure

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,9 +1,22 @@
+import { ImageContainer, ProductContainer, ProductDetails } from '@/src/styles/pages/product';
 import { useRouter } from 'next/router';
 
 export default function Product() {
     const { query } = useRouter();
 
     return (
-        <h1>product: {JSON.stringify(query)}</h1>
+        <ProductContainer>
+            <ImageContainer>
+                
+            </ImageContainer>
+            <ProductDetails>
+                <h1>Camiseta x</h1>
+                <span>R$ 79,99</span>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Enim sapiente deserunt, accusantium ut dicta similique.</p>
+
+                <button>Comprar agora</button>
+
+            </ProductDetails>
+        </ProductContainer>
     )
 }
